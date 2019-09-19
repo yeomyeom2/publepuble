476426e956ac7f03c0504a781a037d93e8ecb292
Yeom Hyejin <yeomyeom2@Yeom-ui-MacBook-Pro.local>
2019-09-19 23:19:53 +0900

    그만하자

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.set('port', process.env.PORT || 80);
 app.post('/', function(req, res){
   res.send('Root');
 });
-
+console.log(1)
 app.post('/chatbot', function(req, res){
     
+console.log(2)
     //console.log(req.body.events[0].source, req.body.events[0].replyToken, req.body.events[0].message.type, req.body.events[0].message.text, req.body.events[0].source.userId);
     //console.log(req.body.events[0].source);
     //console.log(req.body.events[0].replyToken);
