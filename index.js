35e852be8be268af0544be89593536c8ce91f37e
Yeom Hyejin <yeomyeom2@Yeom-ui-MacBook-Pro.local>
2019-09-19 23:42:03 +0900

    ddd

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/', function(req, res){
 
 app.post('/chatbot', function(req, res){
 
-    //console.log(req.body.events[0].source, req.body.events[0].replyToken, req.body.events[0].message.type, req.body.events[0].message.text, req.body.events[0].source.userId);
+    console.log(req.body.events[0].source, req.body.events[0].replyToken, req.body.events[0].message.type, req.body.events[0].message.text, req.body.events[0].source.userId);
     //console.log(req.body.events[0].source);
     //console.log(req.body.events[0].replyToken);
     //console.log(req.body.events[0].message.type);
